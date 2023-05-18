ef9106e463a3abd99a2fcddb7af21dddbc7c511e
Noor Mulla <noor.mulla@teranet.ca>
2023-05-18 20:28:16 +0000

    initial commit k8s cluster terraform project

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "azurerm_resource_group" "main" {
 
 module "aks" {
     source  = "tfe.nmulla.3381802800.workshops.arctiq.ca/NoorWS/aks/azure"
-    version = "1.0.1"
+    version = "1.0.2"
 
     # insert required variables here
     cluster_name          = var.cluster_name
